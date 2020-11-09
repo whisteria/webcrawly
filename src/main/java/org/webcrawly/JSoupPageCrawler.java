90d304d1c2be4525dbf477ef3bfec01a4901844e
mischa czarny <mischa.czarny@gmail.com>
2020-11-09 11:02:44 +0000

    test for the jsoup crawler

--- src/main/java/org/webcrawly/JSoupPageCrawler.java
+++ src/main/java/org/webcrawly/JSoupPageCrawler.java
@@ -4,15 +4,18 @@ import org.jsoup.nodes.Document;
 
 import java.util.Set;
 
+import static java.util.stream.Collectors.toSet;
+
 public class JSoupPageCrawler {
 
+    static Set<String> getLinks(Document document) {
+        return document.select("a[href]").stream().map(n -> n.attr("href")).collect(toSet());
+    }
 
     static Set<String> getImages(Document document) {
-        return null;
+        return document.select("img").stream()
+                .map(n -> n.attr("src")).collect(toSet());
     }
 
-    static Set<String> getLinks(Document document) {
-        return null;
-    }
 
 }
